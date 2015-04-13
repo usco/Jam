1cffcef46dadcff1acacc05d64bea4a5032f5b7b
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-04-13 20:34:33 +0200

    - removed duplicate "meshTools" code (already in other repo)

--- src/meshpp/meshtools/meshTools.js
+++
@@ -1,7 +0,0 @@
-
-import { resizeMesh } from "./resizeMesh.js";
-import { centerMesh } from "./centerMesh.js";
-
-//FIXME: temporary hack
-window.resizeMesh   = resizeMesh;
-window.centerMesh   = centerMesh;
