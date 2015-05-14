285f6ce536f286f26a42b8b0128226710af4c0e8
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-05-14 08:44:37 +0200

    - added "createDesign" action

--- src/actions/designActions.js
+++ src/actions/designActions.js
@@ -1,4 +1,8 @@
 import {createAction} from '../utils/obsUtils'
 
+
+/*create new design */
+export let newDesign$ = createAction();
+
 /*set active designs data/fields */
 export let setDesignData$ = createAction();
