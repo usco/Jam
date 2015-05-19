8e829501f4fafbd1846dc93f26a20f150d17e0fd
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-05-19 21:50:54 +0200

    - cleaned up , refactored, simplified url parsing utilities  * also added query extraction method  * modified api of existing method  * now helper module exports methods directly  * renamed to urlUtils

---
+++ src/utils/urlUtils.js
@@ -0,0 +1,19 @@
+import url from 'url'
+
+export function fetchUriParams(uri, paramName){
+  let targetUrl = window.location.href
+  let params = url.parse(targetUrl, true)
+  let result = params.query
+  //TODO: always return query
+  if(paramName in result) return [].concat( result[paramName] )
+  return []
+}
+
+export function getUriQuery(uri){
+  let uriData = url.parse(uri)
+  let query   = uriData.query
+
+  console.log(uriData, query)
+  return query
+}
+  
