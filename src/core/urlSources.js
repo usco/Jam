e7ad41ebcd82aaad1f9a3c666261386435bbdef3
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-06-10 22:23:05 +0200

    - experimenting with better ways to combine sources of data (url, localstorage etc)

--- src/core/urlSources.js
+++ src/core/urlSources.js
@@ -1,9 +1,14 @@
 import Rx from 'rx'
 
+import {fetchUriParams,getUriQuery,setWindowPathAndTitle}  from '../utils/urlUtils'
+import {setSetting$} from '../actions/appActions'
+import {newDesign$, setDesignData$} from '../actions/designActions'
+
+
 let mainUri    = window.location.href 
 let uriQuery   = getUriQuery(mainUri)
 let designUri = fetchUriParams(mainUri, "designUrl").pop()
-let meshUrli   = fetchUriParams(mainUri, "modelUrl").pop()
+let meshUri   = fetchUriParams(mainUri, "modelUrl").pop()
 let appMode    = fetchUriParams(mainUri, "appMode").pop()
 
 //let designUri$ = new Rx.Observable()
@@ -26,15 +31,52 @@ Rx.Observable
 //load from localstorage in case all else failed
 
 //last but not least, try to load if anything is in the query (shorthand for design uuids)
-if(!designUri && ! meshUrli && !persistentUri && uriQuery)
+/*if(!designUri && ! meshUri && uriQuery)
 {
   //FIXME: this does not seem right ...
   let apiDesignsUri = "https://jamapi.youmagine.com/api/v1/designs/"
   designUri = apiDesignsUri+uriQuery 
 }
 
-setDesignData$({uri:designUri})
-loadDesign$(designUri)
+setDesignData$({uri:designUri})*/
+//loadDesign$(designUri)
 
 //only load meshes if no designs need to be loaded 
-meshUri$(meshUrls) //self.loadMesh(meshUrl) })
+//meshUri$(meshUrls) //self.loadMesh(meshUrl) })
+
+
+
+function getShortDesignUri (uriQuery){
+  if(!uriQuery) return Rx.Observable.just(undefined)
+  let apiDesignsUri = "https://jamapi.youmagine.com/api/v1/designs/"
+  let designUri = apiDesignsUri+uriQuery
+  return Rx.Observable.just( designUri )
+}
+
+let urDesignUri$ = Rx.Observable
+  .just( designUri )
+  .filter(exists)
+
+let lsDesignUri$  = Rx.Observable
+  .just(  localStorage.getItem("jam!-lastDesignUri") )
+  .filter(exists)
+
+let stDesignUri$ = Rx.Observable.merge(
+  urDesignUri$,
+  getShortDesignUri(uriQuery)
+)
+  .shareReplay(1)
+  //.filter(exists)
+
+let foo$ = Rx.Observable.merge(
+  urDesignUri$,//ORDER MATTERS !!!
+  lsDesignUri$,
+  stDesignUri$
+  )
+  .take(1)
+  .subscribe(
+  function(data){
+    console.log("HI THERE : mixed data source",data)
+  })
+
+
