065af92c5790178ad7f4352133926afa73ddeedf
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-06-19 00:07:02 +0200

    - modified sinks to be a method with in/outputs - started refining/tweaking the various sinks (remote, localstorage etc)

--- src/core/sinks.js
+++ src/core/sinks.js
@@ -1,76 +1,93 @@
 //////SINK!!! save changes to design
+/*
 design$
   .distinctUntilChanged()//only save if something ACTUALLY changed
   //.skip(1) // we don't care about the "initial" state
   .debounce(1000)
   //only save when design is set to persistent
-  .filter(design=>design._persistent && (design.uri || design.name) && design._doSave)
+  .filter(design=>design._persistent && (design.uri || design.name) && design._doSave)     
+*/
 
+//////////////////
+//code related to saving etc
+export function serializer(kernel, design$, entities$, annotations$, bom$, combos$, setDesignData$)
+{
 
-design$
-  .distinctUntilChanged()//only save if something ACTUALLY changed
-  //.skip(1) // we don't care about the "initial" state
-  .debounce(1000)
-  //only save when design is set to persistent
-  .filter(design=>design._persistent && (design.uri || design.name) && design._doSave)
-  //staggered approach , do not save the first times
-  .bufferWithCount(2,1)
-  .map(value => value[1])
-  .map(self.kernel.saveDesignMeta.bind(self.kernel))
-  .subscribe(function(def){
-    def.promise.then(function(result){
-      //FIXME: hack for now
-      console.log("save result",result)
-      let serverResp =  JSON.parse(result)
-      let persistentUri = self.kernel.dataApi.designsUri+"/"+serverResp.uuid
-
-      localStorage.setItem("jam!-lastDesignUri",persistentUri)
-      setDesignData$({uri:persistentUri})
+  //testing only
+  design$
+    .distinctUntilChanged()
+    .skip(1)
+    .subscribe(function(design){
+      console.log("design changed",design)
+      let jamLocalData = {
+        persistent:design._persistent,
+        lastDesignUri:design.uri,
+        lastDesignName:design.name
+      }
+      localStorage.setItem("jam!-settings",JSON.stringify(jamLocalData) )
     })
-  })
-  /*.subscribe(function(res){
-    console.log("experimental save result",res)
-  })*/      
 
+  design$
+    .map(design => ({persistent:design._persistent,uri:design.uri}) )
+    .subscribe(function(persitenceInfo){
+      let {persistent,uri} = persitenceInfo
+      kernel.setDesignAsPersistent(persistent,uri)
+    })
 
+  design$
+    .distinctUntilChanged()//only save if something ACTUALLY changed
+    //.skip(1) // we don't care about the "initial" state
+    .debounce(1000)
+    //only save when design is set to persistent
+    .filter(design=>design._persistent && (design.uri || design.name) && design._doSave)
+    //staggered approach , do not save the first times
+    //.bufferWithCount(2,1)
+    //.map(value => value[1])
+    .map(kernel.saveDesignMeta.bind(kernel))
+    .subscribe(function(def){///issue : not purely a sink
+      def.promise.then(function(result){
+        //FIXME: hack for now
+        console.log("save result",result)
+        let serverResp =  JSON.parse(result)
+        let persistentUri = kernel.dataApi.designsUri+"/"+serverResp.uuid
 
-design$
-  .pluck("_persistent")
-  //seperation of "sinks" from the rest
-  .subscribe(function(value){
-    localStorage.setItem("jam!-persistent",value)
-    if(value) self.kernel.setDesignAsPersistent(true)
-  })
+        setDesignData$({uri:persistentUri})
+      })
+    })
 
+  //////SINK!!! save change to assemblies
+  entities$
+    .debounce(500)//don't save too often
+    //only save when design is _persistent
+    .onlyWhen(design$, design=>design._persistent && (design.uri || design.name) && design._doSave)
+    .subscribe(function(entities){
+      kernel.saveAssemblyState(entities.instances)
+    })
+  ///////////
 
-//////SINK!!! save change to assemblies
-entities$
-  .debounce(500)//don't save too often
-  //only save when design is _persistent
-  .onlyWhen(design$, design=>design._persistent && (design.uri || design.name) && design._doSave)
-  .subscribe(function(entities){
-    console.log("GNO")
-    self.kernel.saveBom()//TODO: should not be conflated with assembly
-    self.kernel.saveAssemblyState(entities.instances)
-  })
-///////////
+  bom$
+    .debounce(500)
+    //only save when design is _persistent
+    .onlyWhen(design$, design=>design._persistent && (design.uri || design.name) && design._doSave)
+    .subscribe(function(bom){
+      kernel.saveBom(bom.entries)
+    })
 
-//////SINK!!! save change to assemblies
-annotations$
-  .debounce(500)//don't save too often
-  //only save when design is _persistent
-  .onlyWhen(design$, design=>design._persistent)
-  .subscribe(function (annotations){
-    self.kernel.saveAnnotations(annotations)
-  })
+  //////SINK!!! save change to assemblies
+  annotations$
+    .debounce(500)//don't save too often
+    //only save when design is _persistent
+    .onlyWhen(design$, design=>design._persistent)
+    .subscribe(function (annotations){
+      kernel.saveAnnotations(annotations)
+    })
 
-
-//sink, for saving meshes
-combos$
-  .skip(1)
-  .distinctUntilChanged()
-  .onlyWhen(design$, design=>design._persistent && (design.uri || design.name) && design._doSave)
-  .subscribe(function(cb){
-    console.log("saving mesh")
-    self.kernel.dataApi.saveFile( cb.resource.name, cb.resource._file )
-  })
+  //sink, for saving meshes
+  combos$
+    .skip(1)
+    .distinctUntilChanged()
+    .onlyWhen(design$, design=>design._persistent && (design.uri || design.name) && design._doSave)
+    .subscribe(function(cb){
+      kernel.dataApi.saveFile( cb.resource.name, cb.resource._file )
+    })
+}
