319feb73792950c9e9863c2896898f966776ff7c
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-06-20 16:25:53 +0200

    - removed useless utilities

--- src/utils/otherUtils.js
+++ src/utils/otherUtils.js
@@ -81,38 +81,6 @@ export function getEntity( input ){
   return input.userData.entity
 }
 
-export function extractMeshTransforms(mesh){
-  let attrs = {
-    pos:mesh.position,
-    rot:mesh.rotation,
-    sca:mesh.scale
-  }
-  return attrs
-}
-
-function attributesToArrays(attrs){
-  let output= {}
-  for(let key in attrs){
-    output[key] = attrs[key].toArray()
-  }
-  //special case for rotation
-  if("rot" in attrs)
-  {
-    output["rot"] = output["rot"].slice(0,3)
-  }
-  return output
-}
-
-function setEntityT(attrsAndEntity){
-  let [transforms, entity] = attrsAndEntity      
-  setEntityData$({entity:entity,
-    pos:transforms.pos,
-    rot:transforms.rot,
-    sca:transforms.sca
-  })
-
-  return attrsAndEntity
-}
 /////////////////////////////////
 //ui utils
 export function toggleCursor(toggle, cursorName, element=document.body){
