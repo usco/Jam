744766da0fdf98fb0222769ea58c10f4503a9be5
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-06-24 00:42:20 +0200

    - started importing the bulk of interactions from previous /curent three.js view implementation  * also started refactoring & cleaning up of these interactions

--- src/components/webgl/GlView.js
+++ src/components/webgl/GlView.js
@@ -5,21 +5,158 @@ import Detector from './deps/Detector.js'
 import Cycle from 'cycle-react'
 import React from 'react'
 let Rx = Cycle.Rx
-
+let fromEvent = Rx.Observable.fromEvent
+let merge = Rx.Observable.merge
 
 import {windowResizes,pointerInteractions,preventScroll} from '../../interactions/interactions'
 
+function positionFromCoords(coords){return{position:{x:coords.x,y:coords.y},event:coords}}
+function extractObject(event){ return event.target.object}
 
+function selectionAt(event, container, selector, width, height, dynamicInjector){
+  //log.debug("selection at",event)
+  
+  let rect = container.getBoundingClientRect()
+  let intersects = selector.pickAlt({x:event.clientX,y:event.clientY}, rect, width, height, dynamicInjector)
+
+  //let selectedMeshes = intersects.map( intersect => intersect.object )
+  //selectedMeshes.sort().filter( ( mesh, pos ) => { return (!pos || mesh != intersects[pos - 1]) } )
+
+  //TODO: we are mutating details, is that ok ?
+  //not working in safari etc
+  let outEvent = {}//Object.assign({}, event)
+  outEvent.clientX = event.clientX
+  outEvent.clientY = event.clientY
+  outEvent.offsetX = event.offsetX
+  outEvent.offsetY = event.offsetY
+  outEvent.x = event.x || event.clientX
+  outEvent.y = event.y || event.clientY
+
+
+  outEvent.detail = {}
+  outEvent.detail.pickingInfos = intersects
+
+  return outEvent
+}
+
+function selectMeshes(event, container){
+
+  let intersects = event.detail.pickingInfos
+  let rect = container.getBoundingClientRect()
+
+  let selectedMeshes = intersects.map( intersect => intersect.object )
+  selectedMeshes.sort().filter( ( mesh, pos ) => { return (!pos || mesh != intersects[pos - 1]) } )
+
+  selectedMeshes = selectedMeshes.shift()//we actually only get the best match
+  selectedMeshes = findSelectionRoot(selectedMeshes)//now we make sure that what we have is actually selectable
+
+  if(selectedMeshes){ selectedMeshes = [selectedMeshes] }
+  else{ selectedMeshes = []}
+
+  this.selectedMeshes = selectedMeshes
+
+
+  if(this._prevSelectedMeshes && this._prevSelectedMeshes.length>0){
+      this.transformControls.detach(this._prevSelectedMeshes[0])
+  }
+  if(selectedMeshes.length>0){
+    //if(["0","1","2","3"].indexOf(selectedMeshes[0].typeUid) === -1 )
+    if(this.props.activeTool && ["translate","rotate","scale"].indexOf(this.props.activeTool) > -1 )
+    {
+      this.transformControls.attach(selectedMeshes[0])
+    }
+  }
+
+
+  /*if(this.props.activeTool && ["translate","rotate","scale"].indexOf(this.props.activeTool) > -1 )
+  {
+    if(selectedMeshes.length>0){
+      this.transformControls.attach(selectedMeshes[0])
+    }
+  }else{
+
+    if(this._prevSelectedMeshes && this._prevSelectedMeshes.length>0){
+      this.transformControls.detach(this._prevSelectedMeshes[0])
+    }
+  }*/
+  this._prevSelectedMeshes = this.selectedMeshes
+  this.selectedMeshes$.onNext(selectedMeshes)
+
+  return event
+}
+
+/*TODO:
+- remove any "this", adapt code accordingly 
+- extract reusable pieces of code
+- remove any explicit "actions" like showContextMenu$, hideContextMenu$ etc
+- streamline all interactions
+*/
+
+
+////////////
 function _GlView(interactions, props, self){
   let container$ = interactions.get("#container","ready")
 
   let initialized$ = interactions.subject('initialized').startWith(false) //.get('initialized','click').startWith(false)
+  let reRender$ = Rx.Observable.interval(16) //observable should be the merger of all observable that need to re-render the view?
   let items$  = props.get('items').startWith([])
-
-  let reRender$ = Rx.Observable.interval(16)
   let windowResizes$ = windowResizes(1) //get from intents/interactions ?
+  
+  //singleTaps$ = pointerInteractions( container ).singleTaps$.map( selectionAt )
+  let {singleTaps$, doubleTaps$, contextTaps$, 
+      dragMoves$, zoomIntents$} =  pointerInteractions(container)
+  
+  singleTaps$ = singleTaps$.map( selectionAt ) //stream of taps + selected meshes
+  doubleTaps$ = doubleTaps$.map( selectionAt )
+  contextTaps$ = contextTaps$ //handle context menu type interactions
+    .map( selectionAt )
+    .map( selectMeshes )
+    .map( positionFromCoords )
 
-  console.log("interactions",interactions,"props",props, self.refs)
+  //handle all the cases where events require removal of context menu
+  //ie anything else but context
+  let stopContext$ = merge(singleTaps$, doubleTaps$, dragMoves$)//, zoomIntents$)
+    .take(1)
+    .repeat()
+
+  selectedMeshes$ = singleTaps$.map( selectionAt ) //still needed ?
+
+  let objectsTransforms$ = fromEvent(transformControls, 'objectChange')
+      .map(extractObject)
+
+  //hande all the cases where events require re-rendering
+  reRender$ = reRender$.merge(
+    fromEvent(controls,'change'), 
+    fromEvent(transformControls,'change'), 
+    fromEvent(camViewControls,'change'),
+    selectedMeshes$, 
+    objectsTransform$)
+ 
+
+  
+
+  function areThereSelections(){ return (self.selectedMeshes && self.selectedMeshes.length>0) }
+
+  /*setToTranslateMode$.filter(areThereSelections).subscribe( this.transformControls.setMode.bind(transformControls,"translate") )
+  setToRotateMode$.filter(areThereSelections).subscribe( this.transformControls.setMode.bind(transformControls,"rotate") )
+  setToScaleMode$.filter(areThereSelections).subscribe( this.transformControls.setMode.bind(transformControls,"scale") )*/
+  //from this to  below
+
+  function setTransformsFrom(obses, modes, controls){
+    modes.map( mode => 
+      obs.filter(areThereSelections).subscribe( controls.setMode.bind(controls, mode) )
+    )
+  }
+  setTransformsFrom([setToXXX],transformControls,["translate","rotate","scale"])
+
+
+
+  preventScroll(container)
+
+  //console.log("interactions",interactions,"props",props, self.refs)
+
+
+  //actual 3d stuff
 
   let config = {
       renderer:{
@@ -46,31 +183,22 @@ function _GlView(interactions, props, self){
 
   function setupCamera(){
     var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight; 
-    // camera attributes
     var VIEW_ANGLE = 35, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.01, FAR = 20000
-    // set up camera
     camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR)
-    // add the camera to the scene
-    
-    // the camera defaults to position (0,0,0)
-    //  so pull it back (z = 400) and up (y = 100) and set the angle towards the scene origin
     camera.position.set(0,150,400)
     camera.lookAt(scene.position)
   }
 
   function setupScene(){
-    var light = new THREE.PointLight(0xffffff);
-    light.position.set(0,250,0);
-    scene.add(light);
-    var ambientLight = new THREE.AmbientLight(0x111111);
+    var light = new THREE.PointLight(0xffffff)
+    light.position.set(0,250,0)
+    scene.add(light)
 
-
-    var sphereGeometry = new THREE.SphereGeometry( 50, 32, 16 ); 
-    // use a "lambert" material rather than "basic" for realistic lighting.
-    //   (don't forget to add (at least one) light!)
-    var sphereMaterial = new THREE.MeshLambertMaterial( {color: 0x8888ff} ); 
-    sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    sphere.position.set(100, 50, -50);
+    var sphereGeometry = new THREE.SphereGeometry( 50, 32, 16 ) 
+    var sphereMaterial = new THREE.MeshLambertMaterial( {color: 0x8888ff} );
+    sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+    sphere.position.set(100, 50, -50)
+    scene.add(sphere)
   }
     
   function render(scene, camera){
@@ -105,7 +233,6 @@ function _GlView(interactions, props, self){
 
     container.appendChild( renderer.domElement )
     scene.add(camera)
-    scene.add(sphere)
   }
 
   function handleResize (sizeInfos){
@@ -130,8 +257,8 @@ function _GlView(interactions, props, self){
 
   windowResizes(1).subscribe(  handleResize  )//(data)=>console.log("windowResizes",data))
 
-  //for now we use refs, but once in cycle, we should use virtual dom widgets & co
 
+  //for now we use refs, but once in cycle, we should use virtual dom widgets & co
   let style = {width:"100%",height:"100%"}
   let overlayStyle ={position:'absolute',top:10,left:10}
   let vtree$ =  Rx.Observable.combineLatest(
@@ -169,7 +296,16 @@ function _GlView(interactions, props, self){
   return {
     view: vtree$,
     events:{
-      initialized:initialized$ 
+      initialized:initialized$,
+
+      singleTaps$,
+      doubleTaps$,
+
+      contextTaps$,
+      stopContext$,
+
+      selectedMeshes$,//is this one needed or redundant ?
+      objectsTransforms$
     }
   }
 }
