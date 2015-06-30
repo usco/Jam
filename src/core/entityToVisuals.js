3d88b9796d332e024a74ad782b3bd0ce02b3bedc
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-06-30 09:56:10 +0200

    - more fleshing out of how entityToVisal mapping can be done

--- src/core/entityToVisuals.js
+++ src/core/entityToVisuals.js
@@ -2,6 +2,15 @@ import Rx from 'rx'
 
 //every time a part changes => rerender
 //every time the visual "mapping" of a part changes => re-render
+//visual = entity.map(fn)
+
+/*
+
+bom changes => remoteMeshVisualProvider  
+ie: the user can change the visuals given by the remoteMeshVisualProvider via the bom
+
+
+*/
 
 //lazy load visual representation for each instance, based on type
 //also each "part" instance needs a different mesh instance (because of diffences in color etc)
@@ -11,6 +20,66 @@ import Rx from 'rx'
 //perhaps we need "visualProviders" that register to streams of required visuals, and who only "care"
 //for certain "types" (think, filter)
 
+
+/*
+
+  items$
+    .flatMap(function(items){
+      console.log("items",items)
+      return Rx.Observable.from(items)
+    })
+    .filter(x=> [0,2].indexOf(x.type) > -1 )
+
+  items$
+    .flatMap(function(items){
+      console.log("items",items)
+      return Rx.Observable.from(items)
+
+    })
+    .filter(x=> [4,7].indexOf(x.type) > -1 )
+
+
+  items$
+    .flatMap(function(items){
+      console.log("items",items)
+      return Rx.Observable.from(items)
+
+    })
+    .filter(x=> [3,5].indexOf(x.type) > -1 )
+*/
+
+//helper for mockup, from mdn
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+//method to filter the source data by types : types are arrays of typeUids
+function obsByTypes(srcData, types){
+  return srcData
+    .flatMap(function(items){
+      console.log("items",items)
+      return Rx.Observable.from(items)
+    })
+    .filter(x=> types.indexOf(x.type) > -1 )
+}
+
+/*this function would take raw input data and 
+  - first return a "raw" blockout of a mesh (bounding box, a spiner etc)
+  - emulated the random amount of time it would take to return the actual mesh from
+  a remote location (via xhr or anything etc)
+*/
+function returnBlockOutAndThenMesh(entry){
+  let subJ = new Rx.ReplaySubject()
+  let blockout = {output:"triangle_blockout",entry:JSON.stringify(entry)}
+
+  //send the blockout
+  subJ.onNext(blockout)
+
+  Rx.Observable.timer(getRandomInt(150, 4000), 100).map({output:"triangle",entry:JSON.stringify(entry)}).take(1)
+    .subscribe( data => subJ.onNext(data) )
+
+  return subJ
+}
 
 export function entityToVisuals(){
   console.log("experimental entities to visuals")
@@ -27,38 +96,44 @@ export function entityToVisuals(){
   let items$ = Rx.Observable.just(exampleData)
 
 
+  //------------------------------//
   //remote visual mesh provider
-  let remoteMeshVisualProvider$ = items$
-    .flatMap(function(items){
-      console.log("items",items)
-      return Rx.Observable.from(items)
-    })
-    .filter(x=> [0,2].indexOf(x.type) > -1 )
-  
-  remoteMeshVisualProvider$.subscribe(data => console.log("remoteMeshVisualProvider will deal with ",data))
+  let remoteMeshVisualProvider$ = obsByTypes(items$, [2])
+    .flatMap(returnBlockOutAndThenMesh)
+    /*.flatMap(function(entry){
+      //here we would resolve data
+      return Rx.Observable.timer(getRandomInt(50, 4000), 100).map({output:"triangle",entry:JSON.stringify(entry)}).take(2)
+    })*/
 
+  //remoteMeshVisualProvider$.subscribe(data => console.log("remoteMeshVisualProvider will deal with ",data))
+  remoteMeshVisualProvider$.subscribe(data => console.log("remoteMeshVisualProvider will output ",data))
+
+
+
+  //------------------------------//
   //static visual provider
-  let staticVisualProvider$ = items$
-    .flatMap(function(items){
-      console.log("items",items)
-      return Rx.Observable.from(items)
-
+  let staticVisualProvider$ = obsByTypes(items$, [4,7])
+    .map(function(entry){
+      switch(entry.type){
+        case 4:
+          return {output:"circle",entry:JSON.stringify(entry)}
+        break
+        case 7:
+          return {output:"square",entry:JSON.stringify(entry)}
+        break
+      }
     })
-    .filter(x=> [4,7].indexOf(x.type) > -1 )
 
-  staticVisualProvider$.subscribe(data => console.log("staticVisualProvider will deal with ",data))
+  //staticVisualProvider$.subscribe(data => console.log("staticVisualProvider will deal with ",data))
+  staticVisualProvider$.subscribe(data => console.log("staticVisualProvider will output ",data))
 
-
-    //static visual provider
-  let otherVisualProvider$ = items$
-    .flatMap(function(items){
-      console.log("items",items)
-      return Rx.Observable.from(items)
-
-    })
-    .filter(x=> [3,5].indexOf(x.type) > -1 )
+  //------------------------------//
+  //other visual provider
+  let otherVisualProvider$ = obsByTypes(items$, [3,5])
 
   otherVisualProvider$.subscribe(data => console.log("otherVisualProvider will deal with ",data))
+
+
 
   let visualProviders$ = Rx.Observable.merge(
     remoteMeshVisualProvider$,
