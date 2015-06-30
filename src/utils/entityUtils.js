3b66e94deb80077c4ece7302b0cd4da4886fe1c7
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-06-30 22:48:21 +0200

    - added "hasNoEntity" utility function to entityUtils

--- src/utils/entityUtils.js
+++ src/utils/entityUtils.js
@@ -4,6 +4,12 @@ export function hasEntity( input ){
   return (input && input.userData && input.userData.entity)
 }
 
+//this one should not be needed
+export function hasNoEntity( input ){
+  return !(input && input.userData && input.userData.entity)
+}
+
 export function getEntity( input ){
   return input.userData.entity
 }
+
