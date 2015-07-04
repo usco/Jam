1f413cb832b3370e5b76dead9877e8c29695425a
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-07-04 08:41:12 +0200

    - regrouped all drag & drop interaction helpers in dragAndDrop module

--- src/interactions/dragAndDrop.js
+++ src/interactions/dragAndDrop.js
@@ -4,6 +4,41 @@ let fromEvent = Observable.fromEvent
 
 import {preventDefault,isTextNotEmpty,formatData,exists} from '../utils/obsUtils'
 
+//drag & drop behaviour from dom element
+export function observableDomDragAndDrop(targetEl){
+  let dragOvers$  = fromEvent(targetEl, 'dragover')
+  let drops$  = fromEvent(targetEl, 'drop')
+
+  drops$.subscribe(preventDefault)
+  dragOvers$.subscribe(preventDefault)
+
+  drops$
+    .share()
+
+  let urls$ = drops$
+    .map( (event)=>event.dataTransfer.getData("url") )
+    .filter(isTextNotEmpty)
+    .map( (data)=>formatData([data],"url"))
+
+  let texts$ = drops$
+    .map( (event)=>event.dataTransfer.getData("Text"))
+    .filter(isTextNotEmpty)
+    .map( (data) => formatData([data],"text"))
+  
+  let files$ = drops$
+    .map( (event)=>event.dataTransfer.files )
+    .filter(exists)
+    .map( (data)=>[].slice.call(data))
+    .map( (data) => formatData(data,"file") )
+
+  return Observable.merge(
+    urls$,
+    texts$,
+    files$
+    )
+}
+
+
 
 //drag & drop behaviour 
 export function observableDragAndDrop_old(targetEl, root){
