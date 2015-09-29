340885f99edd51ebf626731d3791a2e13a005fb8
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-09-29 11:10:24 +0200

    - added combineLatestObj to obsUtils

--- src/utils/obsUtils.js
+++ src/utils/obsUtils.js
@@ -25,6 +25,28 @@ export function createAction(paramsMap){
 }
 
 
+/*From https://github.com/staltz/combineLatestObj*/
+export function combineLatestObj(obj) {
+  var sources = [];
+  var keys = [];
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      keys.push(key.replace(/\$$/, ''));
+      sources.push(obj[key]);
+    }
+  }
+  return Rx.Observable.combineLatest(sources, function () {
+    var argsLength = arguments.length;
+    var combination = {};
+    for (var i = argsLength - 1; i >= 0; i--) {
+      combination[keys[i]] = arguments[i];
+    }
+    return combination;
+  })
+}
+
+
+
 export function logNext( next ){
   log.info( next )
 }
