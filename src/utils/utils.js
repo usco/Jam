984b86d3728305c6094c19022d3ac03eb66e1ccb
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-09-30 18:28:38 +0200

    - added uuid generator to utils module

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -26,3 +26,37 @@ export function safeJSONParse(str){
 export function getExtension(fname){
   return fname.substr((~-fname.lastIndexOf(".") >>> 0) + 2).toLowerCase()
 }
+
+//TODO: taken from three.js ,do correct attribution
+export function generateUUID() {
+
+  // http://www.broofa.com/Tools/Math.uuid.htm
+
+  let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split( '' );
+  let uuid = new Array( 36 );
+  let rnd = 0, r;
+
+  return function () {
+
+    for ( let i = 0; i < 36; i ++ ) {
+
+      if ( i == 8 || i == 13 || i == 18 || i == 23 ) {
+
+        uuid[ i ] = '-';
+
+      } else if ( i == 14 ) {
+
+        uuid[ i ] = '4';
+
+      } else {
+
+        if ( rnd <= 0x02 ) rnd = 0x2000000 + ( Math.random() * 0x1000000 ) | 0;
+        r = rnd & 0xf;
+        rnd = rnd >> 4;
+        uuid[ i ] = chars[ ( i == 19 ) ? ( r & 0x3 ) | 0x8 : r ];
+
+      }
+    }
+    return uuid.join( '' )
+  }()
+}
