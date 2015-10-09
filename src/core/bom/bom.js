80fbadbdd17e1fd6e8ba6b15c4d4f7c64bed704a
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-10-09 11:16:27 +0200

    - updated /cleaned up bom system

--- src/core/bom/bom.js
+++ src/core/bom/bom.js
@@ -15,6 +15,70 @@ const defaults = {
   ,byId:{}
 }
 
+
+function addBomEntries(state,input){
+  //console.log("ADDING BOM entries")
+  //FIXME , immutable
+  let newData = input || []
+  if(newData.constructor !== Array) newData = [newData]
+
+  state.entries = state.entries.concat(newData)
+
+  newData.map( entry =>
+     state.byId[entry.uuid]= entry
+    )
+
+  return state
+}
+
+function createBomEntries(state,input){
+  let {combos,types} = input
+  //console.log("I would register something in BOM", input, state)
+
+  let {entries,byId} = state
+
+  let typeUid   = types.latest
+  let type      = types.typeData[typeUid]
+  let entryName =  (type !== undefined ? type.name : undefined) 
+  let meshName  = types.typeUidToMeshName[typeUid]
+
+  if(type){
+    if(! byId[typeUid]){
+      let newEntry={
+        qty: 1,
+        phys_qty: undefined,
+        unit: "EA",
+        description: undefined,
+        uuid: typeUid,
+        implementations: {
+            "default": meshName
+        },
+        name: entryName,
+        version: "0.0.0"
+      }
+      
+      entries = entries.concat( [newEntry] )
+      byId[typeUid] = newEntry
+    }
+    else
+    {
+      byId[typeUid].qty +=1
+    }
+  }
+  
+  return { entries, byId }
+}
+
+//how do we deal with this, as it impacts other data structures ? 
+function removeBomEntries(state,input){
+
+}
+
+function clearBomEntries(state, input){
+  //console.log("clear BOM", input, state)
+  return Object.assign({},defaults)
+}
+
 function makeModifications(intent){
   //intent.partTypes$.subscribe(e=>console.log("partTypes in BOM"))
   //intent.combos$.subscribe(e=>console.log("combos in BOM"))
@@ -22,58 +86,13 @@ function makeModifications(intent){
   let addEntries$ = intent.addBomEntries$
     .filter(exists)
     .map((nData) => (bomData) => {
-      //console.log("ADDING BOM entries")
-      //FIXME , immutable
-      let newData = nData || []
-      if(newData.constructor !== Array) newData = [newData]
-
-      bomData.entries = bomData.entries.concat(newData)
-
-      newData.map( entry =>
-         bomData.byId[entry.uuid]= entry
-        )
-
-      return bomData
+      return addBomEntries(bomData,nData)
     })
 
   let newType$ = intent.partTypes$
     .withLatestFrom(intent.combos$,function(types, combos){ return {combos,types}})
     .map((data) => (bomData) => {
-      let {combos,types} = data
-      //console.log("I would register something in BOM", data, bomData)
-
-      let {entries,byId} = bomData
-
-      let typeUid   = types.latest
-      let type      = types.typeData[typeUid]
-      let entryName =  (type !== undefined ? type.name : undefined) 
-      let meshName  = types.typeUidToMeshName[typeUid]
-
-      if(type){
-        if(! byId[typeUid]){
-          let newEntry={
-            qty: 1,
-            phys_qty: undefined,
-            unit: "EA",
-            description: undefined,
-            uuid: typeUid,
-            implementations: {
-                "default": meshName
-            },
-            name: entryName,
-            version: "0.0.0"
-          }
-          
-          entries = entries.concat( [newEntry] )
-          byId[typeUid] = newEntry
-        }
-        else
-        {
-          byId[typeUid].qty +=1
-        }
-      }
-      
-      return { entries, byId }
+      return createBomEntries(bomData,data)
      })
 
 
@@ -81,14 +100,13 @@ function makeModifications(intent){
   let removeEntries$ = intent.removeEntries$
     .map((data) => (bomData) => {
       //console.log("removeEntries from BOM", data, bomData)
+      return removeBomEntries(bomData,data)
     })
 
   //clear it all
   let clearEntries$ = intent.clearEntries$
     .map((data) => (bomData) => {
-      //console.log("clear BOM", data, bomData)
-
-      return Object.assign({},defaults)
+      return clearBomEntries(bomData,data)
     })
 
   return merge(
