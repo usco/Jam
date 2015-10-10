3c396085ac329595a69090d723b7a2eabc7749e2
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-10-10 12:50:31 +0200

    - fleshing out new version of entityInfos:  * added back name & color visuals  * started adding handling for changes  * added back basic of comments  * other tweaks & cleanups

--- src/components/EntityInfos2.js
+++ src/components/EntityInfos2.js
@@ -7,7 +7,7 @@ let merge = Rx.Observable.merge
 
 import {combineLatestObj, preventDefault,isTextNotEmpty,formatData,exists} from '../utils/obsUtils'
 import {formatNumberTo, absSizeFromBBox} from '../utils/formatters'
-//import Comments from './Comments'
+import Comments from './Comments/Comments'
 
 
 function changeHandler(fieldName, index, event){
@@ -34,15 +34,27 @@ function changeHandler(fieldName, index, event){
   interactions.subject('selectionTransforms$').onEvent(output)
 }
 
-/*
-<Comments className="comments" comments={comments} entity={entity} />
-              {colorInput(entity, changeHandler)}
-              {nameInput(entity,changeHandler)}       
-              {absSizeInput(entity,controlsStep,numberPrecision,changeHandler)}
-              {extraInputs(entity,numberPrecision,changeHandler)}*/
+function nameInput(core){
+   if(core && core.name){
+    return (
+      <span>
+        <input type="text" value={core.name} className="nameInput"/> 
+      </span>
+    )
+  } 
+}
+
+function colorInput(core){
+  if(core && core.color){
+    return (
+      <span>
+        <input type="color" value={core.color} className="colorInput" /> 
+      </span>
+    )
+  } 
+}
 
 function transformInputs(transforms, fieldName, displayName, controlsStep, numberPrecision){
-  console.log("foo")
   let inputs = []
   if(transforms && transforms[fieldName]){
 
@@ -61,25 +73,50 @@ function transformInputs(transforms, fieldName, displayName, controlsStep, numbe
   }
 }
 
+function intent({DOM}){
+
+  const changeName$  = merge(
+    DOM.select(".nameInput").events('change').map(e=>e.target.value)
+    ,DOM.select(".nameInput").events('input').map(e=>e.target.value)
+  )
+  const changeColor$ = DOM.select(".colorInput").events('change').map(e=>e.target.value)
+  
+  return {
+    changeName$
+    ,changeColor$
+  }
+}
+
 function EntityInfos({DOM, props$}, name = '') {
   //let settings$ = props$.pluck('settings').filter(exists).startWith([])
   let comments$ = props$.pluck('comments').filter(exists).startWith(undefined)
+  let core$ = props$.pluck('core')
   let transforms$ = props$.pluck('transforms')
 
   let addComment$          = DOM.select(".comments").events("addComment$").pluck("detail")
 
   //comments$.subscribe(e=>console.log("Comments",e))
+  intent({DOM}).changeName$.subscribe(e=>console.log("changeName",e))
+  intent({DOM}).changeColor$.subscribe(e=>console.log("changeColor",e))
 
   let numberPrecision = 2
   let controlsStep = 0.1
 
-  const vtree$ = combineLatestObj({transforms$,comments$})
-    .map(function({transforms,comments}){
+  const commentsUiProps$ = combineLatestObj({entity:core$.map(e=>e[0]),comments$})
+  const commentsUi = Comments({DOM,props$:commentsUiProps$})
+
+  const vtree$ = combineLatestObj({core$, transforms$, comments:commentsUi.DOM})
+    .distinctUntilChanged()
+    .map(function({core, transforms, comments}){
 
       if(transforms.length>0) transforms = transforms[0]
-        console.log("transforms",transforms)
+      if(core.length>0) core = core[0]
+        //console.log("core,transforms",core,transforms)
 
       return <div className="toolBarBottom entityInfos">
+        {comments}
+        {nameInput(core)}
+        {colorInput(core)}
         {transformInputs(transforms, "pos", "P", controlsStep, numberPrecision)}
         {transformInputs(transforms, "rot", "R", controlsStep, numberPrecision)}
         {transformInputs(transforms, "sca", "S", controlsStep, numberPrecision)}
