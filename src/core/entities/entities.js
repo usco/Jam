3a84262cb3cbba2645afaf4a63431ee2e6a55894
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-10-12 21:04:21 +0200

    chore(entities) removed duplicate "component - based" code

--- src/core/entities/entities.js
+++ src/core/entities/entities.js
@@ -34,53 +34,6 @@ const defaults = {
   byId:{}
 }
 
-/*
-let partInstance = {
-    name: data.name,
-    iuid: generateUUID(),
-    typeUid: data.typeUid,
-    color: "#07a9ff",
-    pos: [ 0, 0, h/2 ],
-    rot: [ 0, 0, 0 ],
-    sca: [ 1, 1, 1 ],
-    bbox:data.bbox
-}*/
-
-//just experimenting with thoughts about component based system
-let partComponents = []
-function makeCoreComponent(typeUid){
-  return  Rx.Observable.just({
-    name: data.name,
-    iuid: generateUUID(),
-    typeUid: typeUid,
-    color: "#07a9ff"
-  })
-}
-
-function makeTransformComponent(){
-  return  Rx.Observable.just({
-    pos: [ 0, 0, 0 ],
-    rot: [ 0, 0, 0 ],
-    sca: [ 1, 1, 1 ]
-  })
-}
-
-function makeBoundingComponent(){
-  return Rx.Observable.just({
-    min:[0,0,0],
-    max:[0,0,0]
-  })
-}
-
-let bounds$ = makeBoundingComponent()
-let transforms$ = makeTransformComponent()
-let combo = Rx.Observable.combineLatest(
-  transforms$
-  ,bounds$,function(transforms,bounds){
-    return {
-      transforms
-      ,bounds}
-  })
 
 ///helper functions
 function reduceToIuidBasedHash(instances){
