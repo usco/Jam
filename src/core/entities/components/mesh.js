3838f6e8535df5574588dd3993866fe32e0aede2
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-10-19 13:30:20 +0200

    refactor(mesh): minor tweaks to ensure mesh component is not immutable

--- src/core/entities/components/mesh.js
+++ src/core/entities/components/mesh.js
@@ -33,7 +33,8 @@ export function makeMeshSystem(){
     , removeComponent}
   let actions   = makeActionsFromApiFns(updateFns)
 
-  let meshes$ = makeModelNoHistory(defaults, updateFns, actions)
+  let meshes$ = makeModelNoHistory(defaults, updateFns, actions, undefined, false)//last flag set to false because we
+  //do not want immutable data for meshes ?
 
   return {meshes$,meshActions:actions}
 }
