f404683b1b6bbe935e5f3c0d40222a4cf7e8a372
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-10-21 18:23:47 +0200

    refactor(intents): old intents2 renamed to intentHelpers

---
+++ src/core/entities/intentHelpers.js
@@ -0,0 +1,53 @@
+import {Rx} from '@cycle/core'
+import {makeInternals, meshResources} from '../meshResources'
+import {extractChanges} from '../../utils/diffPatchUtils'
+
+
+export function entityTypeIntents(sources){
+  let meshSources$ = sources.meshSources$
+  let srcSources$ = sources.srcSources$
+
+  //TODO: get rid of this
+  let assetManager = makeInternals()
+  let meshResources$ = meshResources(meshSources$, assetManager)
+  //meshResources$.subscribe(e=>console.log("meshResources",e))
+
+  //let entityInstance = undefined
+  //return meshResources$.map(e=>e.mesh).map(testHack2).shareReplay(1)
+
+  const clearTypes$  = Rx.Observable.never()
+  const registerTypeFromMesh$ = meshResources$
+
+  return {
+    registerTypeFromMesh$
+    , clearTypes$
+  }
+}
+
+export function entityInstanceIntents(entityTypes$){
+  const baseOps$ = entityTypes$
+    //.distinctUntilChanged()//no worky ?
+    .pluck("typeData")
+    .scan({prev:undefined,cur:undefined},function(acc, x){
+      let cur  = x
+      let prev = acc.cur
+
+      cur = Object.keys(cur).map(function(key){
+        return cur[key]
+      })      
+      return {cur,prev} 
+    })
+    .map(function(typeData){
+      let {cur,prev} = typeData
+
+      let changes = extractChanges(prev,cur)
+    return changes
+  })
+
+  const addInstances$ = baseOps$
+    .pluck("added")
+  
+  return {
+    addInstances$
+  }
+}
