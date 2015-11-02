466f4bf578be6704d4a6f651304e6d2333d4b1cd
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-11-02 14:12:20 +0100

    tweak(Bom): modified toggling behaviour of bom to not close when clicking outside of it

--- src/components/Bom/intent.js
+++ src/components/Bom/intent.js
@@ -10,21 +10,10 @@ export default function intent(DOM){
     .do(e=>e.stopPropagation())
   const removeEntry$ = DOM.select('DOM', '.remove-btn').events('click',true)
     .do(e=>e.stopPropagation())
-
-  //any outside taps close the dialog
-  const outsideTaps$     = fromEvent(window,"click")
-    .map(e=>e.target)
-    .map(function(target){
-      return document.querySelector(".bom").contains(target)
-    })
-    .filter(x=>(x===false))//we only care if it was clicked outside, not in
-  
+ 
   const toggle$  = DOM.select(".bomToggler").events("click")//toggle should be scoped?
     .map(true)
     .scan((acc,val)=>!acc)
-    .merge(outsideTaps$)
-    .takeUntil( outsideTaps$ )
-    .repeat() 
 
   return {
     entryTapped$
