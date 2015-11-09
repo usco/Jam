bf607996cd634de8806916ed5ddd31b3a50aa868
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-11-09 17:52:22 +0100

    refactor(eventDriver): extracted out eventDriver into seperate module

--- src/index.js
+++ src/index.js
@@ -8,49 +8,8 @@ import {postMessageDriver}  from './core/drivers/postMessageDriver'
 import {localStorageDriver} from './core/drivers/localStorageDriver'
 import {addressbarDriver} from './core/drivers/addressbarDriver'
 import browserCapsDriver from './core/drivers/browserCapabilities'
+import eventDriver from './core/drivers/eventDriver'
 
-//test hack/helper driver
-function eventDriver(outgoing$){
-  outgoing$ = outgoing$ || Cycle.Rx.Observable.just({})
-
-  function deep_value(path, obj){
-    try{
-      for (var i=0, path=path.split('.'), len=path.length; i<len; i++){
-        obj = obj[path[i]]
-      }
-      return obj
-    }
-    catch(error){}
-  }
-
-  function makeEventsSelector(source$){
-    return function events(eventName){
-
-      return source$.flatMapLatest(source => {
-        if (!source) {
-          return Rx.Observable.empty()
-        }
-        return source[eventName]
-      }).share()
-    }
-  }
-
-
-  function makeSourceSelector(path){
-    const source$ = outgoing$
-      .map( deep_value.bind(null,path) )
-      .filter(e=>e!==undefined)
-      //.shareReplay(1)
-    const events = makeEventsSelector(source$)
-    return {
-      events 
-    }
-  }
-
-  return {
-    select: makeSourceSelector
-  }
-}
 
 //////////setup drivers
 let domDriver      = makeDOMDriver('#root')
