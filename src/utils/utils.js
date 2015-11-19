1f5e955d1d65bb7dc35c41966253bd7301ca4639
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-11-19 14:36:09 +0100

    feature(utils): added function to extract both name & extension from a uri

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -40,6 +40,13 @@ export function getExtension(fname){
   return fname.substr((~-fname.lastIndexOf(".") >>> 0) + 2).toLowerCase()
 }
 
+export function getNameAndExtension(uri){
+  const _uriElems = uri.split("?");
+  const name = _uriElems.shift().split("/").pop()
+  const ext  = getExtension(uri)
+  return {name,ext}
+}
+
 export function isValidFile(file){
   return ((typeof file !== "undefined" && file !== null) && file instanceof File)
 }
