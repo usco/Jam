1034f67c214920cbee6c8c8b186c92a64bc3f42f
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-11-24 13:02:25 +0100

    feature(tests): added test module for bom (empty husk for now)

---
+++ test/bom.test.js
@@ -0,0 +1,33 @@
+import assert from 'assert'
+
+//TODO: implement
+describe('Bom', function() {
+
+
+  /*qty: 1,
+  phys_qty: undefined,
+  unit: "EA",
+  description: undefined,
+  uuid: typeUid,
+  implementations: {
+      "default": meshName
+  },
+  name: entryName,
+  version: "0.0.0"*/
+
+  describe('removeEntry', function() {
+
+  })
+
+
+  describe('removeEntity',function(){
+
+    it('should update entry counts correctly ', function () {
+      
+      //assert.equal(JSON.stringify(result), JSON.stringify(expected))
+
+    })
+    
+  })
+
+})
