abc8147e15cefc44441a228373c142eae9d93d14
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-12-08 09:34:35 +0100

    fix(): added vertical flip to bufferToPng  to get correct image

--- src/components/webgl/bufferToPng.js
+++ src/components/webgl/bufferToPng.js
@@ -6,8 +6,27 @@ export default function bufferToPng(buffer, width, height, fileName){
     let PNG = require('pngjs').PNG
     let png = new PNG( {width, height} )
 
-    for (let i = 0; i < inBuf.length; ++i) {
+    /*for (let i = 0; i < inBuf.length; ++i) {
       png.data[i] = inBuf[i]
+    }*/
+    //vertical flip
+
+    for (let j = 0; j < height; ++j) {//from https://gist.github.com/bsergean
+      for (let i = 0; i < width; ++i) {
+
+        let k = j * width + i
+        let r = inBuf[4*k]
+        let g = inBuf[4*k + 1]
+        let b = inBuf[4*k + 2]
+        let a = inBuf[4*k + 3]
+
+        let m = (height - j + 1) * width + i
+        png.data[4*m]     = r
+        png.data[4*m + 1] = g
+        png.data[4*m + 2] = b
+        png.data[4*m + 3] = a
+
+      }
     }
     png.pack().pipe(fs.createWriteStream(fileName))
   }
