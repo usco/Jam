b648f153cfb7fe27299a34f076254b2ea350064d
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-02-17 12:05:37 +0100

    chore(utils): added "validUrl" function to validate urls

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -52,6 +52,25 @@ export function isValidFile(file){
   return ((typeof file !== "undefined" && file !== null) && file instanceof File)
 }
 
+//from http://stackoverflow.com/questions/1701898/how-to-detect-whether-a-string-is-in-url-format-using-javascript
+export function isValidUrl(url) {
+
+  /*var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
+      + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@
+      + "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP形式的URL- 199.194.52.184
+      + "|" // 允许IP和DOMAIN（域名）
+      + "([0-9a-z_!~*'()-]+\.)*" // 域名- www.
+      + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // 二级域名
+      + "[a-z]{2,6})" // first level domain- .com or .museum
+      + "(:[0-9]{1,4})?" // 端口- :80
+      + "((/?)|" // a slash isn't required if there is no file name
+      + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$"
+   var re=new RegExp(strRegex)
+   return re.test(url)*/
+    var regexp = /(ftp|http|https|localhost):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+   return regexp.test(url)
+ }
+
 
 //TODO: taken from three.js ,do correct attribution
 export function generateUUID() {
