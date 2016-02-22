6a7c1be693705e27c20d4eeea57eceea6f7dba51
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-02-22 21:32:13 +0100

    feature(diffPatch): - added extractChangesBetweenArrays helper function - helps for better extracting upserts/removes between arrays - updated JsonDiffPatch dependency version

--- src/utils/diffPatchUtils.js
+++ src/utils/diffPatchUtils.js
@@ -12,16 +12,17 @@ let jsondiffpatch = require('jsondiffpatch').create({objectHash:compareHash})
 export function extractChanges(prev, cur){
     let delta = jsondiffpatch.diff(prev, cur)
     //console.log("delta",delta)
-    
     let result = {added:[],removed:[],changed:[]}
 
     if(delta && "_t" in delta){
-        
+
         let removed = []//delta["_0"][0]//delta[0][0]
         let added = []//delta[0][0]//delta[0][1]
 
         Object.keys(delta).map(function(key){
           //console.log("AAA",key)
+          //"_t": "a",	Array delta (member names indicate array indices)
+
           if(key[0]=="_"){
             if(key!=="_a" && key !=="_t")
             {
@@ -32,7 +33,7 @@ export function extractChanges(prev, cur){
           }
 
         })
-        
+
         result.added = toArray(added).filter(i=>i!==undefined)
         result.removed = toArray(removed).filter(i=>i!==undefined)
 
@@ -44,6 +45,67 @@ export function extractChanges(prev, cur){
 
     return result
   }
+
+
+let instance = require('jsondiffpatch').create({
+    objectHash: function(obj, index) {
+      if (typeof obj._id !== 'undefined') {
+        return obj._id;
+      }
+      if (typeof obj.id !== 'undefined') {
+        return obj.id;
+      }
+      if (typeof obj.name !== 'undefined') {
+        return obj.name;
+      }
+      return '$$index:' + index;
+    }
+  })
+
+export function extractChangesBetweenArrays(prev, cur){
+  let delta = instance.diff(prev, cur)
+  //console.log("delta",delta)
+  //console.log("diff",delta)//JSON.stringify(delta, null, 2))
+
+  let result = {added:[],removed:[],changed:[], upserted:[]}
+
+  if(delta && "_t" in delta){
+
+      let removed = []//delta["_0"][0]//delta[0][0]
+      let added = []//delta[0][0]//delta[0][1]
+      let upserted = []
+
+      if(delta["_t"] ===  "a"){
+        //array diff
+        //"_t": "a",	Array delta (member names indicate array indices)
+        Object.keys(delta).map(function(key){
+          if(key !=="_t")
+          {
+            if(key.length>0 && key.indexOf('_')>-1)
+            {
+              //console.log("removed",delta, key)//,key,delta)
+              removed.push(delta[key])
+            }else
+            {
+              //removed or changed
+              //console.log("added or changed",delta, key)
+              upserted.push(delta[key])
+            }
+          }
+
+        })
+      }
+      result.added    = toArray(added).filter(i=>i!==undefined)
+      result.removed  = toArray(removed).filter(i=>i!==undefined)
+      result.upserted = toArray(upserted).filter(i=>i!==undefined)
+      //console.log("added",result.added)
+      //console.log("removed",result.removed)
+  }else if(prev === undefined){//not handled right in the above case for some reason ??
+    result.upserted = cur
+  }
+
+  return result
+}
 
 
 
@@ -69,20 +131,20 @@ export function entityVisualComparer(prev,cur){
     if (!cur)
       return false
 
-    // compare lengths - can save a lot of time 
+    // compare lengths - can save a lot of time
     if (cur.length != prev.length)
       return false
 
     let sortedCur  = cur.sort()
     let sortedPrev = prev.sort()
     for(var i=0;i<cur.length;i++){
-      if(sortedCur[i].typeUid !== sortedPrev[i].typeUid) 
+      if(sortedCur[i].typeUid !== sortedPrev[i].typeUid)
         return false
 
-      if(sortedCur[i].id !== sortedPrev[i].id) 
+      if(sortedCur[i].id !== sortedPrev[i].id)
         return false
 
-     
+
       let curVal = sortedCur[i]
       let preVal = sortedPrev[i]
 
@@ -98,5 +160,5 @@ export function entityVisualComparer(prev,cur){
       if(!allEqual) return false
     }
 
-    return true 
+    return true
   }
