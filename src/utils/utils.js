cb3c9a3ef32afc35ed6c972eb909a45effc129b7
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-03-01 12:12:05 +0100

    feature(stringToBoolean): added stringToBoolean helper

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -105,3 +105,12 @@ export function generateUUID() {
     return uuid.join( '' )
   }()
 }
+
+
+export function stringToBoolean(string){
+ switch(string.toLowerCase().trim()){
+    case "true": case "yes": case "1": return true;
+    case "false": case "no": case "0": case null: return false;
+    default: return Boolean(string);
+  }
+}
