49189ab90fe83132d039c8e00f69d28df76eea09
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-03-10 13:50:01 +0100

    chore(fix): fixed various missing imports etc in comments

--- src/core/comments/intents.js
+++ src/core/comments/intents.js
@@ -1,17 +1,10 @@
 import {mergeActionsByName} from '../../utils/obsUtils'
 
-import actionsFromDOM from './actions/fromDOM'
-import actionsFromAddressbar from './actions/fromAddressbar'
-import actionsFromLocalStorage from './actions/fromLocalStorage'
-import actionsFromPostMessage from './actions/fromPostMessage'
-
+import actionsFromEvents from './actions/fromEvents'
 
 export default function intents(sources){
   const actionsSources = [
-      actionsFromDOM(sources.DOM)
-    , actionsFromAddressbar(sources.addressbar)
-    , actionsFromLocalStorage(sources.localStorage)
-    , actionsFromPostMessage(sources.postMessage)
+      actionsFromEvents(sources.events)
   ]
   return mergeActionsByName(actionsSources)
 }
