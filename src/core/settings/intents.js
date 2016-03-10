8845819c7d6976e5a8c1a401260ce2ae5254e867
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-03-10 11:53:43 +0100

    refactor(settings intents): cleaned up , improved naming for setting intents

--- src/core/settings/intents.js
+++ src/core/settings/intents.js
@@ -1,17 +1,17 @@
 import {mergeActionsByName} from '../../utils/obsUtils'
 
-import settingsIntentFromDOM from './actions/fromDOM'
-import settingsIntentFromAddressbar from './actions/fromAddressbar'
-import settingsIntentFromLocalStorage from './actions/fromLocalStorage'
-import settingsIntentFromPostMessage from './actions/fromPostMessage'
+import actionsFromDOM from './actions/fromDOM'
+import actionsFromAddressbar from './actions/fromAddressbar'
+import actionsFromLocalStorage from './actions/fromLocalStorage'
+import actionsFromPostMessage from './actions/fromPostMessage'
 
 
 export default function intents(sources){
-  const settingActionSources = [
-      settingsIntentFromDOM(sources.DOM)
-    , settingsIntentFromAddressbar(sources.addressbar)
-    , settingsIntentFromLocalStorage(sources.localStorage)
-    , settingsIntentFromPostMessage(sources.postMessage)
+  const actionsSources = [
+      actionsFromDOM(sources.DOM)
+    , actionsFromAddressbar(sources.addressbar)
+    , actionsFromLocalStorage(sources.localStorage)
+    , actionsFromPostMessage(sources.postMessage)
   ]
-  return mergeActionsByName(settingActionSources)
+  return mergeActionsByName(actionsSources)
 }
