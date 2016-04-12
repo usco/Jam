3523d6ff392b47f7bd10dcb433371311a8a4e16c
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-04-12 09:51:06 +0200

    fix(settings persitance): added toolsets to the list of setting entries to ignore when saving to localstorage

--- src/components/main/formatDataForLocalStorage.js
+++ src/components/main/formatDataForLocalStorage.js
@@ -4,7 +4,7 @@ export default function formatDataForLocalStorage ({sources, state$}) {
   return state$ // output to localStorage
     .pluck('settings') // in this case, settings
     .map(function (settings) {
-      return omit(['autoSave', 'autoLoad'], settings) // here put any settings that should NOT be saved
+      return omit(['autoSave', 'autoLoad', 'toolSets'], settings) // here put any settings that should NOT be saved
     })
     .map(s => ({'jam!-settings': JSON.stringify(s)}))
 }
