9d4c2859b35182f469af3f659939022b0945f6cf
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-05-11 09:42:28 +0200

    feat(mirror tool): added all needed extras for settable axis mirroring

--- src/core/entities/actions/fromDOM.js
+++ src/core/entities/actions/fromDOM.js
@@ -2,8 +2,7 @@ export default function intent (DOM, params) {
   // const clearDesign$= DOM.select('.reset').events("click")
   const deleteInstances$ = DOM.select('.delete').events('click').map(undefined)
   const duplicateInstances$ = DOM.select('.duplicate').events('click')
-  const mirrorInstances$ = DOM.select('.mirror').events('click')
-    .tap(e=>console.log('mirrorInstances'))
+  const mirrorInstances$ = DOM.select('.mirror').events('click').map(d=>({axis: 2}))
 
   return {
     deleteInstances$,
