1b2c28cfe0160b883c3ea111212d7e65dee0163a
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-06-13 15:41:43 +0200

    feat(zoomLevel) : added zoomLevel support for zoomInOnBounds

--- src/components/webgl/cameraUtils2.js
+++ src/components/webgl/cameraUtils2.js
@@ -7,42 +7,56 @@ export default function zoomToFitBounds (object, camera, target) {
   }
   let COG = bbox.center()
 
-  //pointCameraTo(COG, target, camera)
-  //camera.lookAt(COG)
+  // pointCameraTo(COG, target, camera)
+  // camera.lookAt(COG)
 
   /*let sphereSize = object.boundingSphere.radius // bbox.size().length() * 0.5
   let distToCenter = sphereSize / Math.sin(Math.PI / 180.0 * camera.fov * 0.5)
 */
   // move the camera backward
 
-  console.log('bbbox',bbox.center())
+  console.log('bbbox', bbox.center())
   target = bbox.center()
-  //let vec = new THREE.Vector3()
+  // let vec = new THREE.Vector3()
   // compute vector from cam position to target
-  //vec.subVectors(camera.position, target)
+  // vec.subVectors(camera.position, target)
   // set that vector's length to the distance to the center
-  //vec.setLength(distToCenter)
+  // vec.setLength(distToCenter)
   // offset camera position by offset distance + target
-  //camera.position.addVectors(vec , target)
+  // camera.position.addVectors(vec , target)
 
   const height = 100
 
   const correctForDepth = 1.7
-  const radius =  object.boundingSphere.radius
-  const center =  object.boundingSphere.center
-  const distance = height / 2 / Math.tan(Math.PI * fov / 360)//center.distanceTo(camera.position) - radius
-  const offset = new THREE.Vector3().subVectors(center, camera.position)
+  const radius = object.boundingSphere.radius
+  const center = object.boundingSphere.center
   const targetOffset = new THREE.Vector3().subVectors(center, camera.target)
 
-
   var realHeight = Math.abs(bbox.max.y - bbox.min.y)
-  var fov = 2 * Math.atan(realHeight * correctForDepth / ( 2 * distance )) * ( 180 / Math.PI )
-  //camera.fov = fov
+  //  const distance = height / 2 / Math.tan(Math.PI * fov / 360)// center.distanceTo(camera.position) - radius
 
-  //move camera
-  console.log('offset',targetOffset)
+  // var fov = 2 * Math.atan(realHeight * correctForDepth / ( 2 * distance )) * ( 180 / Math.PI )
+  // camera.fov = fov
+
+  var fov = camera.fov * (Math.PI / 180)
+  // Calculate the camera distance
+  var distance = Math.abs(realHeight / Math.sin(fov / 2))
+
+  //var maxDim = Math.max(w, h)
+  //var aspectRatio = w / h
+  //var distance = maxDim / 2 / aspectRatio / Math.tan(Math.PI * fov / 360)
+
+  // move camera
+  console.log('offset', targetOffset)
   camera.position.add(targetOffset)
   camera.target.copy(center)
+
+  let vec = new THREE.Vector3()
+  vec.subVectors(camera.position, camera.target)
+  vec.normalize()
+  vec.setLength(center.distanceTo(camera.position) - radius*4)
+  console.log('distance', distance, 'vec', vec)
+  camera.position.sub(vec)
 
   camera.updateProjectionMatrix()
 
