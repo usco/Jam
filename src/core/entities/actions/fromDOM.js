be8b72fda3fde8709b244c96a5b19f22ceea7b06
Tamara Hogenhout <t.hogenhout@ultimaker.com>
2016-08-22 15:32:43 +0200

    intent

--- src/core/entities/actions/fromDOM.js
+++ src/core/entities/actions/fromDOM.js
@@ -2,6 +2,7 @@ export default function intent (DOM, params) {
   // const clearDesign$= DOM.select('.reset').events("click")
   const deleteInstances$ = DOM.select('.delete').events('click').map(undefined)
   const duplicateInstances$ = DOM.select('.duplicate').events('click')
+  const resetScaling$ = DOM.select('.resetScaling').events('click')
   const mirrorInstances$ = DOM.select('.mirror-x').events('click').map(d=>({axis: 0}))
     .merge(DOM.select('.mirror-Y').events('click').map(d=>({axis: 1})))
     .merge(DOM.select('.mirror-Z').events('click').map(d=>({axis: 2})))
@@ -9,6 +10,7 @@ export default function intent (DOM, params) {
   return {
     deleteInstances$,
     duplicateInstances$,
+    resetScaling$,
     mirrorInstances$
   }
 }
