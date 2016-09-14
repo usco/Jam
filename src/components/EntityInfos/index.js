62e8eb521053f9ec0fdf0d38554421d108cee032
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-09-14 10:21:43 +0200

    feat(abs scaling): absolute scaling works, way simpler now - complitely modified, striped down and solved absolute scaling (see algorithm on top) - extracted lots of boilerplate code into helper functions

--- src/components/EntityInfos/index.js
+++ src/components/EntityInfos/index.js
@@ -5,14 +5,48 @@ import view from './view'
 import intent from './intent'
 import assign from 'fast.js/object/assign' // faster object.assign
 
-import {pluck, head} from 'ramda'
+import { pluck, head } from 'ramda'
 
+/* pipeiline
+  ask geometry for actual "raw" bounds
+  ask transforms for scale
+  "real bounds" => rawBounds * scale
 
-function reduceToAverage (acc, cur){
-  if(!acc) return cur
+  wanna update scale based on bounds ?
+  - what is the current real bounds?
+  - what is raw bounds ?
+  - compute new scale
+
+  VOILA!
+*/
+
+function reduceToAverage (acc, cur) {
+  if (!acc) return cur
   return [acc[0] + cur[0], acc[1] + cur[1], acc[2] + cur[2]].map(x => x * 0.5)
 }
-//
+
+function averageAndSetByFieldname(fieldName, changed, data){
+  const average = pluck(fieldName)(data)
+    .reduce(reduceToAverage, undefined)
+
+  let value = Object.assign([], currentAvg)
+  value[changed.idx] = changed.val
+  return {value, average}
+}
+
+// "spread" data to a list, by ids value over a list of ids
+function spreadToAll (fieldNames){
+  return function(data) {
+    const {ids} = data
+    return ids.map(function (id) {
+      let result = {}
+      fieldNames.forEach(f => {result[f] = data[f]})
+      result['id'] = id
+      return result
+    })
+  }
+}
+
 function model (props$, actions) {
   return props$.map(function (props) {
     const {comments, meta, transforms, settings} = props
@@ -28,13 +62,14 @@ function refineActions (props$, actions) {
   const selections$ = props$.pluck('selections')
   const transforms$ = props$.pluck('transforms')
     .filter(exists)
+    .filter(x => x.length > 0)
   const bounds$ = props$.pluck('bounds')
     .filter(exists)
-    .filter(x=>x.length>0)
+    .filter(x => x.length > 0)
   const activeTool$ = props$.pluck('settings', 'activeTool')
     .distinctUntilChanged()
 
-  //bounds$.forEach(e=>console.log('GNAGNAbounds',e[0].min,e[0].max))
+  // bounds$.forEach(e=>console.log('GNAGNAbounds',e[0].min,e[0].max))
 
   const changeBoundsBase$ = actions.changeBounds$
     .withLatestFrom(bounds$, selections$, function (changed, bounds, selections) {
@@ -45,165 +80,49 @@ function refineActions (props$, actions) {
       newValue[changed.idx] = changed.val
       return {oldValue: currentAvg, value: newValue, ids: selections}
     })
+    .filter(exists)
     .shareReplay(1)
 
   const changeBounds$ = changeBoundsBase$
-    .map(function(data){
-      const {value, ids} = data
-      return ids.map(function(id){
-        return {value, id}
-      })
-    })
+    .map(spreadToAll(['value', 'trans']))
     .share()
 
   const boundsChange$ = changeBoundsBase$
     .withLatestFrom(bounds$, transforms$, selections$, function (changedBounds, bounds, transforms, selections) {
       const currentAvg = changedBounds.oldValue
       const newValue = changedBounds.value
-      const scaleAvg = pluck('sca')(transforms)
-        .reduce(reduceToAverage, undefined)
-
-      //console.log('currentAvg', currentAvg, 'newAvg', newValue)
-      //FIXME SCALE calculation is wrong, offseting everything
-      let diff = [newValue[0] - currentAvg[0], newValue[1] - currentAvg[1], newValue[2] - currentAvg[2]]
-      let scaleChange = [diff[0] /currentAvg[0], diff[1] / currentAvg[1], diff[2] / currentAvg[2]]
-        .map((x, idx)=>1+x)
-
-      console.log('diff',diff[0], newValue[0], currentAvg[0], scaleChange[0])
-      //return {value: [1,1,1], trans: 'sca', ids: selections}
+      let scaleChange = [newValue[0]/ currentAvg[0], newValue[1]/ currentAvg[1],
+        newValue[2] / currentAvg[2]]
       return {value: scaleChange, trans: 'sca', ids: selections}
     })
+    //.tap(e => console.log('scaleChange', e.value[0]))
 
-    /*.withLatestFrom(bounds$, transforms$, selections$, function (changed, bounds, transforms, selections) {
-      console.log('bounds', bounds)
-      let currentAvg = pluck('size')(bounds)
-        .reduce(function (acc, cur) {
-          if(!acc) return cur
-          return [acc[0] + cur[0], acc[1] + cur[1], acc[2] + cur[2]].map(x => x * 0.5)
-        }, undefined)
-
-      console.log('currentAvg', currentAvg)
-      let scaleAvg = pluck('sca')(transforms)
-        .reduce(function (acc, cur) {
-          if(!acc) return cur
-          return [acc[0] + cur[0], acc[1] + cur[1], acc[2] + cur[2]].map(x => x * 0.5)
-        }, undefined)
-
-      let newValue = Object.assign([] ,currentAvg)
-      newValue[changed.idx] = changed.val
-      let diff = [newValue[0] - currentAvg[0], newValue[1] - currentAvg[1], newValue[2] - currentAvg[2]]
-      let scaleChange = [diff[0] /currentAvg[0], diff[1] / currentAvg[1], diff[2] / currentAvg[2]]
-        .map((x, idx)=>x+scaleAvg[idx])
-
-      //console.log('diff',diff[0], newValue[0], currentAvg[0])//, scaleAvg[0], scaleChange[0])
-
-      return {value: scaleChange, trans: 'sca', ids: selections}
-    })*/
-    .tap(e=>console.log('scaleChange', e.value[0]))
-
-  const changeTransformsBase$ = actions.changeTransforms$
+  const changeTransforms$ = actions.changeTransforms$
     .withLatestFrom(transforms$, selections$, function (changed, transforms, selections) {
-      let _transforms = JSON.parse(JSON.stringify(transforms)) // FIXME : this is needed because of mutation bug
-      let avg = pluck(changed.trans)(_transforms)
-        .reduce(function (acc, cur) {
-          if(!acc) return cur
-          return [acc[0] + cur[0], acc[1] + cur[1], acc[2] + cur[2]].map(x => x * 0.5)
-        }, undefined)
+      let avg = pluck(changed.trans)(transforms)
+        .reduce(reduceToAverage, undefined)
+      avg[changed.idx] = changed.val
 
-      if(avg){
-        avg[changed.idx] = changed.val
-      }
       return {value: avg, trans: changed.trans, ids: selections}
     })
     .merge(boundsChange$)
-    .filter(x=> x.value !== undefined)
-    .map(function(data){
-      const {value, trans, ids} = data
-      return ids.map(function(id){
-        return {value: value, trans, id}
-      })
-    })
+    .filter(x => x.value !== undefined)
+    .map(spreadToAll(['value', 'trans']))
     .share()
-    //.tap(e=>console.log('transforms',e))
+    // .tap(e=>console.log('transforms',e))
 
-
-
-  /*const selectionTransforms$ =
-    selections$.distinctUntilChanged()
-    .withLatestFrom(transforms$, (selections, transforms) => ({selections, transforms}))
-    .combineLatest(activeTool$, function ({selections, transforms}, activeTool) {
-      const trans = {'translate': 'pos', 'rotate': 'rot', 'scale': 'sca'}[activeTool]
-      let _transforms = JSON.parse(JSON.stringify(transforms)) // FIXME : this is needed because of mutation bug
-      let avg = pluck(trans)(_transforms)
-        .reduce(function (acc, cur) {
-          if(!acc) return cur
-          return [acc[0] + cur[0], acc[1] + cur[1], acc[2] + cur[2]].map(x => x * 0.5)
-        }, undefined)
-      return {value: avg, trans: trans, ids: selections, cmd: 'reset'}
-    })
-    .filter(x=> x.value !== undefined && x.trans !== undefined)
-
-  const changeTransformsF$ = changeTransformsBase$.merge(selectionTransforms$)
-
-
-  function combiner (stream) {
-    return stream.scan(function (acc, changed) {
-      if(!acc) {
-        let diff = [0, 0, 0]
-        if(changed.id) {
-          diff[changed.id] = changed.value[changed.id]
-        }
-        //console.log('diff', diff, 'new', changed.value)
-        return [{diff, value: changed.value, trans: changed.trans, ids: changed.ids}]
-      }else{
-        if(changed.cmd === 'reset')// selection changed, reset
-        {
-          //console.log('selection changed, reseting')
-          let diff = [0, 0, 0]
-          //console.log('diff', diff, 'new', changed.value)
-          return [{diff, value: changed.value, trans: changed.trans, ids: changed.ids}]
-        }
-
-        if(acc.length < 2) { // adding a new one
-          acc.push(changed)
-        }
-        if(acc.length === 2) {
-          const [first, second] = acc
-          const diff = [second.value[0] - first.value[0], second.value[1] - first.value[1], second.value[2] - first.value[2]]
-          //console.log('diff', diff, 'old', first.value, 'new', second.value)
-          return [{diff, value: second.value, trans: second.trans, ids: second.ids}]
-        }
-      }
-    }, undefined)
-    .filter(x => x.length === 1)
-    .map(head)
-    .map(function(data){
-      const {diff, trans, ids} = data
-      return ids.map(function(id){
-        return {value: diff, trans, id}
-      })
-    })
-    .tap(e=>console.log('input FIRST',e))
-  }
-
-  const changeTransforms$ = combiner(changeTransformsF$)*/
-  const changeTransforms$ = changeTransformsBase$
   const resetScaling$ = actions.resetScaling$
-      .withLatestFrom(selections$, function (_, selections) {
-        return {ids: selections}
-      })
-      .filter(exists)
-      .map(function(data){
-        const {ids} = data
-        return ids.map(id => ({id}))
-      })
+    .withLatestFrom(selections$, function (_, selections) {
+      return {ids: selections}
+    })
+    .filter(exists)
+    .map(spreadToAll([]))
 
   return {
     changeMeta$: actions.changeMeta$,
     changeBounds$,
     changeTransforms$,
-    resetScaling$
-  }
+  resetScaling$}
 }
 
 function EntityInfos ({DOM, props$}, name = '') {
@@ -217,8 +136,7 @@ function EntityInfos ({DOM, props$}, name = '') {
       changeMeta$,
       changeBounds$,
       changeTransforms$,
-      resetScaling$
-    }
+    resetScaling$}
   }
 }
 
